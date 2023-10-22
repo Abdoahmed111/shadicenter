6c53fb49d9c09a45ca8bf40d8e3dcd359444187e
Abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2023-10-22 10:04:51 +0300

    generate sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,11 @@
+export default async function sitemap() {
+    return [
+        { url: "https://shadicenter.net", lastModified: new Date() },
+        { url: "https://shadicenter.net/dr-alaa", lastModified: new Date() },
+        {
+            url: "https://shadicenter.net/sabah-center",
+            lastModified: new Date(),
+        },
+        { url: "https://shadicenter.net/products", lastModified: new Date() },
+    ];
+}
